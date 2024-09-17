d5c76db24c741be7d7f0f67324c1510b6b5905b5
Tim Dobson <hello@tdobson.net>
2024-09-17 16:07:33 +0100

    somehow, this worked this time

--- views/wp_orders_users_fakedb.sql
+++
@@ -1,1 +0,0 @@
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `wp_orders_users_fakedb` AS select `wp_postmeta`.`post_id` AS `post_id`,`wp_postmeta`.`meta_value` AS `meta_value` from `wp_postmeta` where `wp_postmeta`.`meta_key` = '_customer_user' and `wp_postmeta`.`post_id` in (select `wp_posts`.`ID` from `wp_posts` where `wp_posts`.`post_type` = 'shop_order')

d3e29fd1680844bc0663b5eebbac622b1987834d
Tim Dobson <hello@tdobson.net>
2024-09-17 17:38:13 +0100

    fix: removed unnecessary index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-console.log('CavingCrew Database Scripts');
-console.log('Available commands:');
-console.log('  npm run pull - Retrieve and save MySQL views from the server');
-console.log('  npm run push - Update MySQL views on the server');
